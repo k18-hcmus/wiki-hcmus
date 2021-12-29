a7be0a26ae2d4138f41dfa0d1ec79b29152c05f1
nhanlinh1478 <luongtrongnhan1082000@gmail.com>
2021-12-29 23:01:15 +0700

    update_file

--- shared/constants.js
+++ shared/constants.js
@@ -1,22 +1,3 @@
-export const CONTRIBUTION_CONST = {
-  TIER: {
-    Iron: 15,
-    Bronze: 50,
-    Silver: 200,
-    Gold: 600,
-    Platinum: 1200,
-    Diamond: 2000,
-    Master: 3200,
-    Challenger: 5000,
-  },
-  RECIEVED_PTS: {
-    postReview: 15,
-    postComment: 3,
-    upvote: 1,
-    downvote: -1,
-  },
-}
-
 export const INFORMATION_CONST = {
   GENDER: ['Male', 'Female', 'Unknown'],
 }
@@ -164,3 +145,57 @@ export const STATES_CONST = [
     label: 'PENDING',
   },
 ]
+export const CONTRIBUTION_CONST = {
+  TIER: {
+    Iron: 15,
+    Bronze: 50,
+    Silver: 200,
+    Gold: 600,
+    Platinum: 1200,
+    Diamond: 2000,
+    Master: 3200,
+    Challenger: 5000,
+  },
+  RECIEVED_PTS: {
+    postReview: 15,
+    postComment: 3,
+    upvote: 1,
+    downvote: -1,
+  },
+}
+
+export const STATUS_POST = {
+  Publish: {
+    value: 'Publish',
+    label: 'Publish',
+  },
+  Report: {
+    value: 'Report',
+    label: 'Report',
+  },
+  Refused: {
+    value: 'Refused',
+    label: 'Refused',
+  },
+}
+export const CHOICE_OPTION = {
+  EveryOne: {
+    label: 'EveryOne',
+    value: 1,
+  },
+  Individual: {
+    label: 'Individual',
+    value: 2,
+  },
+}
+export const BUTTON_POST = {
+  Save: {
+    label: 'Save',
+  },
+  Undo: {
+    label: 'Undo',
+  },
+  Delete: {
+    label: 'Delete',
+  },
+}
