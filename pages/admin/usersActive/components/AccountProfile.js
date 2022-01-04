0efe022db7c49b74eda4818eb9b8873a611bcc5b
nhanlinh1478 <luongtrongnhan1082000@gmail.com>
2022-01-04 21:55:32 +0700

    Dencentralization_user_api

--- pages/admin/usersActive/components/AccountProfile.js
+++ pages/admin/usersActive/components/AccountProfile.js
@@ -13,32 +13,37 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import axiosClient from '../../../../axiosClient'
 import _ from 'lodash'
+import { ROLE_STATUS } from '../../../../shared/moderator-constants'
+import { CONTRIBUTION_CONST } from '../../../../shared/constants'
 export const AccountProfile = (props) => {
   const [user, setUser] = useState([])
   const [posts, setPosts] = useState()
   const router = useRouter()
   const { id } = router.query
   const [contribution, setContribution] = useState()
-  const [disable, setDisable] = useState(true)
+  const [disable, setDisable] = useState(false)
   useEffect(() => {
     async function FetchUser() {
       const response = await axiosClient.get(`/account-users/${id}`)
       setUser(response.data)
       setPosts(response.data.Posts)
+      console.log('user', response.data)
       let c = _.get(response.data.Contributions[0], 'Value')
       setContribution(c)
       //get contribution to use decentralization user
-      if (contribution >= 150 && user.Posts.length) {
-        setDisable(false)
-      }
     }
     FetchUser()
   }, [])
 
-  const handleDecentralizationUser = () => {
-    console.log('contri:', contribution, 'length:', user.Posts.length)
-    if (contribution >= 150 && user.Posts.length) {
+  const handleClick = async () => {
+    try {
+      const userId = user.User.id
+      const response = await axiosClient.put(`/users/${userId}`, {
+        role: ROLE_STATUS.MODERATOR.value,
+      })
       setDisable(true)
+    } catch (error) {
+      console.log(error)
     }
   }
   return (
@@ -52,7 +57,7 @@ export const AccountProfile = (props) => {
           }}
         >
           <Avatar
-            src={user.avatar}
+            src={user.AvatarURL}
             sx={{
               height: 64,
               mb: 2,
@@ -63,7 +68,7 @@ export const AccountProfile = (props) => {
             {user.DisplayName}
           </Typography>
           <Typography color="textSecondary" variant="body2">
-            {`${user.Email} `}
+            {user.Email}
           </Typography>
           <Typography color="textSecondary" variant="body2">
             {user.Phone}
@@ -72,19 +77,20 @@ export const AccountProfile = (props) => {
       </CardContent>
       <Divider />
       <CardActions>
-        {/* <Button color="primary" fullWidth variant="text" variant="outlined">
-          Upload picture
-        </Button> */}
-        <Button
-          color="primary"
-          fullWidth
-          variant="text"
-          variant="outlined"
-          disabled={disable}
-          onClick={handleDecentralizationUser}
-        >
-          Decentralization
-        </Button>
+        {contribution >= CONTRIBUTION_CONST.TIER.Silver && user.Posts.length ? (
+          <Button
+            color="primary"
+            fullWidth
+            variant="text"
+            variant="outlined"
+            onClick={handleClick}
+            disabled={disable}
+          >
+            Decentralization
+          </Button>
+        ) : (
+          <Typography></Typography>
+        )}
       </CardActions>
     </Card>
   )
