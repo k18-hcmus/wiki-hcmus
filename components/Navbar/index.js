14d9d764fee82071674fae9d293bae57fa685800
Nguyen Tran <67467375+nguyenGALAXY@users.noreply.github.com>
2022-01-05 10:01:18 +0700

    fix merge conflix (#26)

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -32,7 +32,7 @@ import { userLogout, getUserAuth } from '../../redux/slices/userSlice'
 import { toggleLoginForm } from '../../redux/slices/authSlice'
 import axiosClient from '../../axiosClient'
 import Link from 'next/link'
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from '@mui/icons-material/Close'
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
@@ -74,45 +74,44 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }))
 
 const SearchDrop = styled(Box)({
-  width: "300px",
-  height: "210px",
-  borderRadius: "10px",
+  width: '300px',
+  height: '210px',
+  borderRadius: '10px',
   zIndex: 5,
-  position: "absolute",
-  background: "#fff",
-  color: "#000",
-  top: "65px",
-  left: "170px"
+  position: 'absolute',
+  background: '#fff',
+  color: '#000',
+  top: '65px',
+  left: '170px',
 })
 
 const SearchResult = styled(ListItemButton)({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  alignItems: "center"
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 })
 
 const Tag = styled(Box)({
-  width: "50px",
-  height: "30px",
-  backgroundColor: "#b1b0ad",
-  borderRadius: "10px",
-  marginRight: "5px",
-  color: "#fff",
-  fontSize: "15px",
-  fontWeight: "bold",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
+  width: '50px',
+  height: '30px',
+  backgroundColor: '#b1b0ad',
+  borderRadius: '10px',
+  marginRight: '5px',
+  color: '#fff',
+  fontSize: '15px',
+  fontWeight: 'bold',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 })
 
 const CloseBox = styled(IconButton)({
-  position: "absolute",
-  right: "5px",
-  top: "-5px",
-  zIndex: 10
+  position: 'absolute',
+  right: '5px',
+  top: '-5px',
+  zIndex: 10,
 })
-
 
 export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = useState(null)
@@ -121,273 +120,270 @@ export default function PrimarySearchAppBar() {
   const isMenuOpen = Boolean(anchorEl)
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
   const isLoggedIn = useSelector(getUserAuth)
+  const [search, setSearch] = useState('')
+  const [isAuth, setIsAuth] = useState(false)
+  const router = useRouter()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsAuth(!!localStorage.getItem('token')) //Check authenticate
+    }
+  }, [])
 
   const handleOpenLogin = () => {
     dispatch(toggleLoginForm())
-    const [search, setSearch] = useState("")
-    const [isAuth, setIsAuth] = useState(false)
-    const router = useRouter()
-    const dispatch = useDispatch()
-    useEffect(() => {
-      if (typeof window !== 'undefined') {
-        setIsAuth(!!localStorage.getItem('token')) //Check authenticate
-      }
-    }, [])
-
-    const handleCloseLogin = () => {
-      setIsAuth(!!localStorage.getItem('token'))
-      setIsLoginOpen(false)
-    }
-
-    const handleOpenRegister = (event) => {
-      event.preventDefault()
-      setIsRegisterOpen(true)
-    }
-    const handleCloseRegister = () => {
-      setIsRegisterOpen(false)
-    }
-    const handleLogOut = () => {
-      localStorage.clear()
-      axiosClient.defaults.headers.common['Authorization'] = ``
-      router.push('/')
-      dispatch(userLogout())
-      handleMenuClose()
-    }
-    const handleProfileMenuOpen = (event) => {
-      setAnchorEl(event.currentTarget)
-    }
-
-    const handleMobileMenuClose = () => {
-      setMobileMoreAnchorEl(null)
-    }
-
-    const handleMenuClose = () => {
-      setAnchorEl(null)
-      handleMobileMenuClose()
-    }
-
-    const handleMobileMenuOpen = (event) => {
-      setMobileMoreAnchorEl(event.currentTarget)
-    }
-
-    const handleSearch = (event) => {
-      setSearch(event.target.value)
-    }
-
-    const handleCloseDrop = () => {
-      setSearch("")
-    }
-
-    const handleSearchTitle = () => {
-      router.push(`/search?title=${search}`)
-      setSearch("")
-    }
-
-    const handleSearchTag = () => {
-      router.push(`/search?tag=${search}`)
-      setSearch("")
-    }
-
-    const handleSearchUser = () => {
-      router.push(`/search?userName=${search}`)
-      setSearch("")
-    }
-
-    const handleSubmitSearch = (e) => {
-      e.preventDefault()
-      router.push(`/search?post=${search}`)
-      setSearch("")
-    }
-
-
-
-    const menuId = 'primary-search-account-menu'
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        id={menuId}
-        sx={{ mt: '40px' }}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={isMenuOpen}
-        onClose={handleMenuClose}
-      >
-        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-        <MenuItem onClick={handleLogOut}>Log out</MenuItem>
-      </Menu>
-    )
-
-    const mobileMenuId = 'primary-search-account-menu-mobile'
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        id={mobileMenuId}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={isMobileMenuOpen}
-        onClose={handleMobileMenuClose}
-      >
-        <MenuItem>
-          <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-            <Badge badgeContent={17} color="error">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={handleProfileMenuOpen}>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <p>Profile</p>
-        </MenuItem>
-      </Menu>
-    )
-
-    const pages = [
-      { page: 'Home', href: '/' },
-      { page: 'Teacher', href: '/' },
-      { page: 'Subject', href: '/' },
-    ]
-
-    return (
-      <Box sx={{ flexGrow: 1 }}>
-        <Login />
-        <Register open={isRegisterOpen} handleClose={handleCloseRegister} />
-        <AppBar position="static">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ display: { xs: 'none', sm: 'block' } }}
-            >
-              {/* TODO: Change to icon */}
-              ICON PLACE
-            </Typography>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <form onSubmit={handleSubmitSearch} ><StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} onChange={(e) => handleSearch(e)} /></form>
-            </Search>
-            {
-              (search !== "") && (
-                <SearchDrop sx={{ boxShadow: 3 }}>
-                  <CloseBox onClick={handleCloseDrop}>
-                    <CloseIcon />
-                  </CloseBox>
-                  <List>
-                    <ListItem>
-                      <SearchResult onClick={handleSearchTitle}>
-                        <ListItemText primary={search} />
-                        <Tag>Title</Tag>
-                      </SearchResult>
-                    </ListItem>
-                    <ListItem>
-                      <SearchResult onClick={handleSearchTag}>
-                        <ListItemText primary={search} />
-                        <Tag>Tag</Tag>
-                      </SearchResult>
-                    </ListItem>
-                    <ListItem>
-                      <SearchResult onClick={handleSearchUser}>
-                        <ListItemText primary={search} />
-                        <Tag>User</Tag>
-                      </SearchResult>
-                    </ListItem>
-                  </List>
-                </SearchDrop>
-              )
-            }
-            {/* TODO: Link Button to direct page */}
-            <Box style={{ marginRight: 'auto' }} sx={{ display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <CustomLink key={page.page} href={page.href} page={page.page} />
-              ))}
-            </Box>
-
-            {/* Create New Post Link Button */}
-            {!isLoggedIn ? (
-              <Box>
-                <Grid container direction="row" spacing={2}>
-                  <Grid item>
-                    <Button variant="contained" onClick={handleOpenLogin}>
-                      LOGIN
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button variant="contained" color="warning" onClick={handleOpenRegister}>
-                      SIGN UP
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Box>
-            ) : (
-              <>
-                <Box>
-                  <CustomLink href="/CreatePost" page="Create Post" />
-                </Box>
-                {/* Desktop Nav */}
-                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                  <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-                    <Badge badgeContent={17} color="error">
-                      <NotificationsIcon />
-                    </Badge>
-                  </IconButton>
-                  <IconButton
-                    size="large"
-                    edge="end"
-                    aria-label="account of current user"
-                    aria-controls={menuId}
-                    aria-haspopup="true"
-                    onClick={handleProfileMenuOpen}
-                    color="inherit"
-                  >
-                    <AccountCircle />
-                  </IconButton>
-                </Box>
-                {/* Mobile Nav */}
-                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                  <IconButton
-                    size="large"
-                    aria-label="show more"
-                    aria-controls={mobileMenuId}
-                    aria-haspopup="true"
-                    onClick={handleMobileMenuOpen}
-                    color="inherit"
-                  >
-                    <MoreIcon />
-                  </IconButton>
-                </Box>
-              </>
-            )}
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
-      </Box>
-    )
   }
+
+  const handleOpenRegister = (event) => {
+    event.preventDefault()
+    setIsRegisterOpen(true)
+  }
+  const handleCloseRegister = () => {
+    setIsRegisterOpen(false)
+  }
+  const handleLogOut = () => {
+    localStorage.clear()
+    axiosClient.defaults.headers.common['Authorization'] = ``
+    router.push('/')
+    dispatch(userLogout())
+    handleMenuClose()
+  }
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null)
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null)
+    handleMobileMenuClose()
+  }
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget)
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const handleCloseDrop = () => {
+    setSearch('')
+  }
+
+  const handleSearchTitle = () => {
+    router.push(`/search?title=${search}`)
+    setSearch('')
+  }
+
+  const handleSearchTag = () => {
+    router.push(`/search?tag=${search}`)
+    setSearch('')
+  }
+
+  const handleSearchUser = () => {
+    router.push(`/search?userName=${search}`)
+    setSearch('')
+  }
+
+  const handleSubmitSearch = (e) => {
+    e.preventDefault()
+    router.push(`/search?post=${search}`)
+    setSearch('')
+  }
+
+  const menuId = 'primary-search-account-menu'
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      id={menuId}
+      sx={{ mt: '40px' }}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      <MenuItem onClick={handleLogOut}>Log out</MenuItem>
+    </Menu>
+  )
+
+  const mobileMenuId = 'primary-search-account-menu-mobile'
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  )
+
+  const pages = [
+    { page: 'Home', href: '/' },
+    { page: 'Teacher', href: '/' },
+    { page: 'Subject', href: '/' },
+  ]
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Login />
+      <Register open={isRegisterOpen} handleClose={handleCloseRegister} />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            {/* TODO: Change to icon */}
+            ICON PLACE
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <form onSubmit={handleSubmitSearch}>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+                onChange={(e) => handleSearch(e)}
+              />
+            </form>
+          </Search>
+          {search !== '' && (
+            <SearchDrop sx={{ boxShadow: 3 }}>
+              <CloseBox onClick={handleCloseDrop}>
+                <CloseIcon />
+              </CloseBox>
+              <List>
+                <ListItem>
+                  <SearchResult onClick={handleSearchTitle}>
+                    <ListItemText primary={search} />
+                    <Tag>Title</Tag>
+                  </SearchResult>
+                </ListItem>
+                <ListItem>
+                  <SearchResult onClick={handleSearchTag}>
+                    <ListItemText primary={search} />
+                    <Tag>Tag</Tag>
+                  </SearchResult>
+                </ListItem>
+                <ListItem>
+                  <SearchResult onClick={handleSearchUser}>
+                    <ListItemText primary={search} />
+                    <Tag>User</Tag>
+                  </SearchResult>
+                </ListItem>
+              </List>
+            </SearchDrop>
+          )}
+          {/* TODO: Link Button to direct page */}
+          <Box style={{ marginRight: 'auto' }} sx={{ display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <CustomLink key={page.page} href={page.href} page={page.page} />
+            ))}
+          </Box>
+
+          {/* Create New Post Link Button */}
+          {!isLoggedIn ? (
+            <Box>
+              <Grid container direction="row" spacing={2}>
+                <Grid item>
+                  <Button variant="contained" onClick={handleOpenLogin}>
+                    LOGIN
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" color="warning" onClick={handleOpenRegister}>
+                    SIGN UP
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          ) : (
+            <>
+              <Box>
+                <CustomLink href="/CreatePost" page="Create Post" />
+              </Box>
+              {/* Desktop Nav */}
+              <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+                  <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </Box>
+              {/* Mobile Nav */}
+              <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="show more"
+                  aria-controls={mobileMenuId}
+                  aria-haspopup="true"
+                  onClick={handleMobileMenuOpen}
+                  color="inherit"
+                >
+                  <MoreIcon />
+                </IconButton>
+              </Box>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  )
 }
